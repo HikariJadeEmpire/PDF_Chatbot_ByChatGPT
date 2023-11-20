8f1f6718f800268a2a6936da26ead96bb27400e3
HikariJadeEmpire <rastuniing@hotmail.com>
2023-11-20 15:08:49 +0700

    remove unrequired libraries

--- Chatbot_llm.py
+++ Chatbot_llm.py
@@ -17,13 +17,12 @@ from PIL import Image
 import streamlit as st
 
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
-from langchain.prompts import PromptTemplate
 
 import os
 from dotenv import load_dotenv, find_dotenv
